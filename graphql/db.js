97893f6ad03a89924423fea9c5ec671bed6a9396
omnmala <omnmala@gmail.com>
2018-09-08 19:16:21 +0900

    wrapping a rest api with graphql

--- graphql/db.js
+++ graphql/db.js
@@ -1,60 +1,31 @@
-const API_URL = "https://yts.am/api/v2/list_movies.json"
+import fetch from "node-fetch";
 
+const API_URL = "https://yts.am/api/v2/list_movies.json/?"
 
-
-
-
-
-
-
-
-
-export let movies = [
-  {
-    id: '0',
-    name: 'Star Wars = The new one',
-    score: 1
-  },
-  {
-    id: '1',
-    name: 'Avengers - The new one',
-    score: 28
-  },
-  {
-    id: '2',
-    name: 'The Godfather 1',
-    score: 99
-  },
-  {
-    id: '3',
-    name: 'Logan',
-    score: 2
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
-];
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === String(id));
-  return filteredMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter(movie => movie.id !== String(id));
-  if(movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+  if (rating > 0) {
+    REQUEST_URL += `minimum_rating=${rating}`;
   }
-};
+  return fetch(`${REQUEST_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+}
 
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+/*
+export const getMovies = (limit, rating) => {
+  return fetch(`${API_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 };
+*/
+
+/*
+export const getMovies = (limit, rating) =>
+  fetch(`${API_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+*/
